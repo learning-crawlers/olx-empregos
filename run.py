a7f9613215e8a4f5d8c129218e8c845684dd27e7
Alex B. Santos <alexbenincasa@ymail.com>
2019-11-10 23:21:14 -0300

    atualizações

--- run.py
+++ run.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 #######################################
 ##  Empregos OLX SP
-##    Busca de empregos no olx com titulo, anunciante, contato e descrição
+##    Busca de empregos no olx com url, titulo, anunciante, contato, descricao, local, data
 ##
 ##    Author: Alex Benincasa Santos 
 ##    Mail: alexbenincasa@ymail.com
 ##    2019
 #######################################
 
+import os
 import re
 import time
+import json
+import hashlib
 import requests
+from datetime import datetime as date
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By as by
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,60 +27,127 @@ from bs4 import BeautifulSoup
 bin_path = 'C:\\www\\olx-empregos\\geckodriver.exe'
 
 # função para pegar os dados do emprego
-def get_dados_emprego(url):
+def salvarVaga(url):
 	try:
-		browser = webdriver.Firefox(executable_path=bin_path)
+		options = Options()
+		#options.headless = True
+		
+		browser = webdriver.Firefox(executable_path=bin_path, options=options)
 		wait = WebDriverWait(browser, 15)
-
 		browser.get(url)
 
-		titulo = browser.find_element_by_css_selector('h1').text
+		titulo = browser.find_element_by_css_selector('h1').text.replace('/','')
+		# print(f'[{titulo}] titulo')
 
 		contato = []
-		for div in browser.find_elements_by_css_selector('div'):
-			if 'DescriptionMarginTop' in div.get_attribute('class'):
+		# print(f'[{titulo}] contato')
+		for div in browser.find_elements_by_css_selector('#content div > div div'):
+			if 'ver número' in div.text:
 
 				# clicar em todos os números da descrição
 				for a in div.find_elements_by_css_selector('a'):
 					if '9' in a.text:
 						a.click()
 
-				for span in div.find_elements_by_css_selector('span'):
+				# print(f'[{titulo}] svg in a.innerHTML')
+				
+				# print(f'[{titulo}] span in p')
+				for span in div.find_elements_by_css_selector('p'):
 					if '9' in span.text:
 
 						# regex de números brasileiros
-						regex_result = re.findall(r'((\([0-9]{2}(\) )?|[0-9]{2}( )?|([0-9]{2})?)?[0-9]{5}( |-)?[0-9]{4})',span.text)
+						regex_result = re.findall(r'((\([0-9]{2}(\) )?|[0-9]{2}( )?|([0-9]{2})?)?[0-9]{5}( |-)?[0-9]{4})',span.text.replace('.',''))
 						for tel in regex_result:
 							if len(tel[0])>4:
 								contato.append(tel[0]) 
+
 						break
+
 					break
+
+				for a in browser.find_elements_by_css_selector('#content a'):
+					#print(a.get_attribute('innerHTML'))
+					if 'svg' in a.get_attribute('innerHTML'):
+						a.click()
 
 				mail = re.findall(r'[\w\.-]+@[\w\.-]+', div.text)
 				if mail:
 					contato.append(mail[0])
 
 				descricao = div.text
+
+		for div in browser.find_elements_by_css_selector('#content div > div > div div'):
+			if 'Publicado em' in div.text:
+				ano = date.now().year
+				data_published = div.text.replace('Publicado em ','').replace(' às ',f'/{ano} ').split('\n')[0]
+				data_published = date.strptime(data_published, '%d/%m/%Y %H:%M').strftime('%Y-%m-%dT%H:%M:00')
 				break
 
+		# print(f'[{titulo}] descricao = div.text')
 		if len(contato)==0:
 			browser.quit()
-
-		# esperando os dados serem carregados
-		wait.until(ec.presence_of_element_located((by.CSS_SELECTOR, '#miniprofile')))
+			raise ValueError('Vaga não tem contato')
 
 		anunciante = browser.find_element_by_css_selector('#miniprofile span').text
+		# print(f'[{titulo}] anunciante')
 
-		return { 'titulo': titulo, 'anunciante': anunciante, 'contato': contato, 'descricao': descricao }
+		vaga = {}
+		vaga['Url'] = url.strip()
+		vaga['Titulo'] = titulo.strip()
+		vaga['Anunciante'] = anunciante.strip()
+		vaga['Contato'] = contato
+		vaga['Descricao'] = descricao.strip()
+		vaga['Local'] = ''
+		vaga['Data'] = data_published
 
-	except Exception as err:
-		print(err)
-	finally:
+		local = None
+		has_municipio = False
+		has_bairro = False
+		for info in browser.find_elements_by_css_selector('dt,dd'):
+			if 'Município' in info.text:
+				has_municipio = True
+				continue
+
+			if 'Bairro' in info.text:
+				has_bairro = True
+				continue
+
+			if has_municipio:
+				vaga['Local'] = info.text.strip()
+				has_municipio = False
+				local = vaga['Local']
+
+			if has_bairro:
+				bairro = info.text.strip()
+				if local:
+					vaga['Local'] += ' - '+bairro
+				elif bairro:
+					vaga['Local'] = bairro
+
+				has_bairro = False
+
+		anuncio = vaga['Titulo']+vaga['Anunciante']+vaga['Local']
+
 		browser.quit()
 
+		# o hash vai identificar a vaga
+		hash = 'data/'+hashlib.sha224(anuncio.strip().encode('utf-8')).hexdigest()
+		fname = hash+f'-{titulo}.json'.lower()
+		if os.path.isfile(fname):
+			raise ValueError('Vaga já foi cadastrada')
 
-lista_links = []
-for page in range(1,10):
+		# salvar o arquivo com os dados da vaga
+		with open(fname, mode="w") as f:
+			f.write(json.dumps(vaga, indent=4))
+
+	except Exception as e:
+		print(f'[{titulo}] '+str(e))
+		browser.quit()
+
+	exit()
+
+links = []
+for page in range(1,20):
 	# sp.olx.com.br Busca em São Paulo
 	# o={} Paginação
 	result = requests.get('https://sp.olx.com.br/vagas-de-emprego?o={}'.format(page))
@@ -84,18 +156,13 @@ for page in range(1,10):
 	soup = BeautifulSoup(result.text, features='html.parser')
 
 	for li in soup.select('ul.list li.item a.OLXad-list-link-featured'):
-		lista_links.append(li['href'])
+		links.append(li['href'])
 
-empregos = []
 # max_workers=3 pra não travar seu pc
 # threading no python para executar mais rápido (de forma assíncrona)
 with ThreadPoolExecutor(max_workers=3) as executor:
-	for thread in as_completed({ executor.submit(get_dados_emprego,url): url for url in lista_links }):
+	for thread in as_completed({ executor.submit(salvarVaga,url): url for url in links }):
 		try:
-			empregos.append(thread.result())
-		except Exception as err:
-			print(err)
-
-for emprego in empregos:
-	if emprego:
-		print(emprego)
+			thread.result()
+		except Exception as e:
+			print(e)
